f942cc59a9ba8297ecafad807aff826d87bcd1ae
willemhuijzer <willemhuijzer2@hotmail.com>
2022-12-14 23:48:24 +0100

    Update

--- network-pruning.py
+++ network-pruning.py
@@ -12,14 +12,18 @@ class NetworkPruning(BNReasoner):
 
     def execute_pruning(self):
         print("Evidence and Query:", self.evidence, self.query)
-        self.edge_pruning(self.evidence)
+        for x in self.evidence:
+            print("Print X: ", x)
+            self.edge_pruning(x)
         self.leaf_node_pruning_loop(self.evidence ^ self.query)
         return
 
 
-example_query = {"X"}
-example_evidence = {"J"}
-bnReasoner = NetworkPruning('testing/lecture_example2.BIFXML', example_query, example_evidence)
+example_query = {"Wet Grass?"}
+example_evidence = {"Winter?", "Rain?"}
+bnReasoner = NetworkPruning('testing/lecture_example.BIFXML', example_query, example_evidence)
 pruned_network = copy.deepcopy(bnReasoner)  # why the fuck here
 pruned_network.execute_pruning()
 bnReasoner.bn.draw_structure()
+pruned_network.bn.draw_structure()
+
